ccb04bdb795e081ee90806ae56d959ff08a346c3
MKM <191165849+MKM2025@users.noreply.github.com>
2025-04-04 02:04:51 +0300

    Fix: Remove experimental features from next.config.js

--- next.config.js
+++ next.config.js
@@ -48,14 +48,6 @@ const nextConfig = {
       },
     ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig 
